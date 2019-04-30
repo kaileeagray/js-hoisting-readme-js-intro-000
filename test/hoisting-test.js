9bf6b7bdd51acc5f0f1bdd9de5dbd67fd5756ab5
Kailee Gray <kaileeagray@gmail.com>
2019-04-30 17:54:18 +0000

    Automatically backed up by Learn

--- test/hoisting-test.js
+++ test/hoisting-test.js
@@ -4,7 +4,7 @@
 
 const chai = require('chai')
 const fs = require('fs')
-// const jsdom = require('mocha-jsdom')
+const jsdom = require('mocha-jsdom')
 const path = require('path')
 const spies = require('chai-spies')
 
